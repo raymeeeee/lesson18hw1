8bdc894189a45d0e76b66787b0f338a9c758091c
raymeeeee <daniapoluektov@gmail.com>
2024-06-11 20:02:50 +0100

    create array of 5 workers

--- lesson18hw1/lesson18hw1.cpp
+++ lesson18hw1/lesson18hw1.cpp
@@ -12,5 +12,12 @@ struct Worker {
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int size = 5;
+    Worker* arr = new Worker[5]{
+        {"Bobenko", 1979, "Director", 100000, "Higher"},
+        {"Uschenko", 1960, "Worker", 30000, "Lower"},
+        {"Tkachenko", 1959, "Worker", 40000, "Middle"},
+        {"Melnik", 1969, "Secretary", 60000, "Higher"},
+        {"Pivovarov", 1980, "Manager", 80000, "Higher"}
+    };
 }
