07ce9ba646440a14524f74d7ad52a11ae841c9c6
raymeeeee <daniapoluektov@gmail.com>
2024-06-11 19:55:42 +0100

    Add project files.

---
+++ lesson18hw1/lesson18hw1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+struct Worker {
+
+    char surname[50] = "";
+    int birthdate = 0;
+    char position[50] = "";
+    double salary = 0;
+    char education[50] = "";
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
